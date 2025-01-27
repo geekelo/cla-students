0fa6a9d3155c8b728614e2486688c1ffc34c4345
olanikegloria <olanikegloria2020@gmail.com>
2025-01-27 18:35:15 +0100

    fixed linters error

--- src/components/portal/intenalPages/profile/index.js
+++ src/components/portal/intenalPages/profile/index.js
@@ -12,17 +12,31 @@ function Profile() {
 
       <div className="profile-details">
         <h3 className="profile-name">John Doe</h3>
-        <p className="profile-cohort"><span className="profile-label">Cohort:</span> Fall 2024</p>
-        <p className="profile-email"><span className="profile-label">Email:</span> john.doe@example.com</p>
-        <p className="profile-phone"><span className="profile-label">Phone: </span>+1234567890</p>
+        <p className="profile-cohort">
+          <span className="profile-label">Cohort:</span>
+          Fall 2024
+        </p>
+        <p className="profile-email">
+          <span className="profile-label">Email:</span>
+          john.doe@example.com
+        </p>
+        <p className="profile-phone">
+          <span className="profile-label">Phone: </span>
+          +1234567890
+        </p>
 
         <div className="profile-actions">
           <a href="/certificate" className="profile-link">
-            <FontAwesomeIcon icon={faDownload} style={{ marginRight: '5px' }} /> Download Certificate
+            <FontAwesomeIcon icon={faDownload} style={{ marginRight: '5px' }} />
+            Download Certificate
           </a>
           <div>
-            <button type="button" className="profile-btn">Edit Profile</button>
-            <button type="button" className="profile-btn">Logout</button>
+            <button type="button" className="profile-btn">
+              Edit Profile
+            </button>
+            <button type="button" className="profile-btn">
+              Logout
+            </button>
           </div>
         </div>
       </div>
