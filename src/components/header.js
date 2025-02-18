b54f54496eb26d84d083e8a64a8dad84968fdc66
Eloghene Otiede <efuelight12@gmail.com>
2025-02-18 15:26:16 +0100

    Fix - Add home link to header (#56)

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,11 @@ function Header() {
       <nav className="nav">
         <ul className="nav-list">
           <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
             <Link to="/about" className="nav-link">
               About
             </Link>
