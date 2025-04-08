3aa1fa9c1b640d0e956f1f52b2d987b2461c24e5
Eloghene Otiede <efuelight12@gmail.com>
2025-04-08 21:18:35 +0100

    Change Progress Board to Dashboard (#82)
    * IMprove interfac of homepage on mobile screens
    
    * Change progreess to dashboard
    
    ---------
    
    Co-authored-by: geekelo <efueliht12@gmail.com>

--- src/components/portal/intenalPages/dashboard/index.js
+++ src/components/portal/intenalPages/dashboard/index.js
@@ -99,8 +99,8 @@ function Dashboard() {
   return (
     <div className="dashboard-container">
       <div className="dashboard-header">
-        <h1 className="dashboard-title">Your Progress Board</h1>
-        <p className="dashboard-subtitle">Quick overview of your progress and updates</p>
+        <h1 className="dashboard-title">Dashboard</h1>
+        <p className="dashboard-subtitle">Quick overview of your performance</p>
       </div>
 
       <div className="dashboard-stats">
